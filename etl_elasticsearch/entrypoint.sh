b9e80d2bf45f4050c5f47daff1b92adf22814b8f
samtonck <samtonck@gmail.com>
2024-05-16 15:51:38 +0300

    8 | test data

--- etl_elasticsearch/entrypoint.sh
+++ etl_elasticsearch/entrypoint.sh
@@ -8,17 +8,6 @@ wait_for_postgres() {
    echo "Postgres is ready!"
 }
 
-wait_for_elastic() {
-  tries=1
-  echo "Waiting for Elastisearch..."
-  until curl "elasticsearch:$ELASTIC_PORT/_cluster/health?wait_for_status=yellow&timeout=30s"; do
-    >&2 echo "Elastisearch is unavailable - waiting for it... 😴 ($tries)"
-    sleep 1
-    tries=$(expr $tries + 1)
-  done
-  echo "Elasticsearch is ready!"
-}
-
 wait_for_redis() {
    echo "Waiting for Redis..."
    while ! nc -z redis $REDIS_PORT; do
@@ -27,10 +16,21 @@ wait_for_redis() {
    echo "Redis is ready!"
 }
 
+wait_for_elastic() {
+  tries=1
+  echo "Waiting for Elastisearch..."
+  until curl "elasticsearch:$ELASTIC_PORT/_cluster/health?wait_for_status=yellow&timeout=30s"; do
+    >&2 echo "Elastisearch is unavailable - waiting for it... 😴 ($tries)"
+    sleep 5
+    tries=$(expr $tries + 1)
+  done
+  echo "Elasticsearch is ready!"
+}
+
 
 wait_for_postgres
-wait_for_elastic
 wait_for_redis
+wait_for_elastic
 
 echo "All containers are ready"
 
---
+++ etl_elasticsearch/test.py
@@ -0,0 +1,45 @@
+from elasticsearch import Elasticsearch
+from configs.settings import ElasticSettings, settings
+
+
+def get_all_documents():
+    """
+    Получает все документы из индекса Elasticsearch.
+
+    Returns:
+    list: Список всех документов из индекса.
+    """
+    elastic_settings = ElasticSettings()
+    index_name = settings.elastic_movies_index
+    query = {
+        "query": {
+            "match_all": {}
+        }
+    }
+    url = f"{elastic_settings.hosts[0].scheme}://{elastic_settings.hosts[0].host}:{elastic_settings.hosts[0].port}"
+
+    es = Elasticsearch(
+        [url],
+        request_timeout=elastic_settings.timeout,
+        max_retries=elastic_settings.max_retries,
+        retry_on_timeout=elastic_settings.retry_on_timeout
+    )
+
+    result = es.search(index=index_name, body=query, scroll='1m', size=1000)
+
+    all_documents = []
+    hits = result['hits']['hits']
+    scroll_id = result['_scroll_id']
+    all_documents.extend(hits)
+    while hits:
+        result = es.scroll(scroll_id=scroll_id, scroll='1m')
+        hits = result['hits']['hits']
+        all_documents.extend(hits)
+    return all_documents
+
+
+if __name__ == "__main__":
+    all_documents = get_all_documents()
+    print(f"Total documents retrieved: {len(all_documents)}")
+    for doc in all_documents:
+        print(doc)
