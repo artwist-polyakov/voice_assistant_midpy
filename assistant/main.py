a46732c030bbc348c6b90bfdb3d35e68fbaed2eb
samtonck <samtonck@gmail.com>
2024-05-16 12:14:38 +0300

    8 | flake8 + isort

--- assistant/main.py
+++ assistant/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
 import sentry_sdk
+from fastapi import FastAPI
 
 sentry_sdk.init(
-    dsn="https://4f00dac96532d2ac5f94f2bd9453cc39@o4507097467846656.ingest.de.sentry.io/4507226490011728",
+    dsn="https://4f00dac96532d2ac5f94f2bd9453cc39@o4507097467846656.ingest.de.sentry.io/4507226490011728",  # noqa
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     traces_sample_rate=1.0,
@@ -14,6 +14,7 @@ sentry_sdk.init(
 
 app = FastAPI()
 
+
 @app.get("/sentry-debug")
 async def trigger_error():
-    division_by_zero = 1 / 0
+    division_by_zero = 1 / 0  # noqa
