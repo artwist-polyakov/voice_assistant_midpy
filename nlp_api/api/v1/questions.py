fc070990bb14910e32e347fc8ba86f9c0ce8f2ef
Aleksandr Poliakov <master@artwist.ru>
2024-05-22 21:31:27 +0300

    инжектит сервис и встравивает его в логику системы

--- nlp_api/api/v1/questions.py
+++ nlp_api/api/v1/questions.py
@@ -5,6 +5,10 @@ from api.v1.models.requests.nlp_request import QuestionParam
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 
+from db.models.requests.search_request import SearchRequest
+from service.base_service import BaseService
+from service.dummy_service import get_dummy_service
+
 router = APIRouter()
 
 
@@ -16,12 +20,27 @@ router = APIRouter()
 async def ask_question(
         external_user_id: str,
         external_session_id: str,
-        params: QuestionParam = Depends()
+        params: QuestionParam = Depends(),
+        search_service: BaseService = Depends(get_dummy_service)
 ) -> JSONResponse:
     logging.info(f"Question received: external_user_id={external_user_id}, "
                  f"external_session_id={external_session_id}, "
-                 f"params={params.dict()}")
+                 f"params={params.model_dump()}")
+    result = await search_service.proceed_request(SearchRequest(
+        external_user_id=external_user_id,
+        external_session_id=external_session_id,
+        query=params.text
+    ))
+    logging.info(f"Result: {result}")
+    if result:
+        film_name = result['data'][0]['title']
+        film_rating = result['data'][0]['imdb_rating']
+        return JSONResponse(
+            status_code=HTTPStatus.OK,
+            content={
+                'result': f'Кажется вас заинтересует фильм "{film_name}" с рейтингом {film_rating}'}
+        )
     return JSONResponse(
-        status_code=HTTPStatus.OK,
-        content={'message': f'Question received: params={params.dict()}'}
+        status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+        content={'result': "Запрос не удался"}
     )
