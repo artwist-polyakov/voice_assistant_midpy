9530477de515bd3791902c2921c11b1faf0826c0
Aleksandr Poliakov <master@artwist.ru>
2024-05-27 23:55:25 +0300

    добавляет external message в запрос

--- nlp_api/db/models/requests/search_request.py
+++ nlp_api/db/models/requests/search_request.py
@@ -4,4 +4,5 @@ from db.models.requests.base_request import BaseRequest
 class SearchRequest(BaseRequest):
     external_user_id: str
     external_session_id: str
+    external_message_id: str
     query: str
