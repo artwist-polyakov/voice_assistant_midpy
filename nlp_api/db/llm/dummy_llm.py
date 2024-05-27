192656af41f945d25c99a22141fa86e00342df95
Aleksandr Poliakov <master@artwist.ru>
2024-05-27 23:54:40 +0300

    меняет бейз реквест на сёрч реквест к интерфейсе llm

--- nlp_api/db/llm/dummy_llm.py
+++ nlp_api/db/llm/dummy_llm.py
@@ -1,12 +1,13 @@
 from db.llm.base_llm import BaseLLM
-from db.models.requests.base_request import BaseRequest
 from elasticsearch_dsl.query import MultiMatch, Query
+
+from db.models.requests.search_request import SearchRequest
 
 index_name = str
 
 
 class DummyLLM(BaseLLM):
-    async def get_query(self, request: BaseRequest) -> (index_name, Query):
+    async def get_query(self, request: SearchRequest) -> (index_name, Query):
         if "вездны" in request.query and "войн" in request.query:
             return ('movies',
                     MultiMatch(query="star wars", fields=['title^5', 'description']))
