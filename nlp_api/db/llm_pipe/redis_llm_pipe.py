c76c72ef272884e543d4d147f9c0f34977fbf697
Aleksandr Poliakov <master@artwist.ru>
2024-05-30 23:02:03 +0300

    создаёт наследника редиса, который читает пайплайн

---
+++ nlp_api/db/llm_pipe/redis_llm_pipe.py
@@ -0,0 +1,23 @@
+from db.llm_pipe.base_llm_pipe import BaseLLMPipe
+from db.llm_pipe.redis_core import RedisCore
+
+
+class RedisLLMPipe(BaseLLMPipe, RedisCore):
+
+    @RedisCore.initialize
+    async def get_keys_with_values(self, prefix: str) -> dict:
+        keys = await self._redis.keys(f"{prefix}*")
+        values = await self._redis.mget(*keys)
+        return dict(zip(keys, values))
+
+    @RedisCore.initialize
+    async def put_cache(self, key: str, value: str, expired=0) -> None:
+        pass
+
+    async def close(self):
+        if self._redis is not None:
+            await self._redis.close()
+            self._redis = None
+
+    async def get_cache(self, key: str) -> str:
+        return await self._redis.get(key)
