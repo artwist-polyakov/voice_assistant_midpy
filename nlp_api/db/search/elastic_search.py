2088c0f3a042584c19585b407437002ddc1f524a
Aleksandr Poliakov <master@artwist.ru>
2024-05-31 22:02:18 +0300

    индекс всегда movies

--- nlp_api/db/search/elastic_search.py
+++ nlp_api/db/search/elastic_search.py
@@ -31,7 +31,7 @@ class ElasticSearch(BaseSearch):
             index, search = await self._llm.get_query(request)
             logging.info(f"Got query: {search.to_dict()}")
             logging.info(f"Got index: {index}")
-            result = await self._elastic.search(index=index, body=search.to_dict())
+            result = await self._elastic.search(index='movies', body=search.to_dict())
             data = []
             for element in result['hits']['hits']:
                 data.append(element['_source'])
