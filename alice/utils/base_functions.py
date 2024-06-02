676d2eb598090c0d6f0593d951c15d926be50214
Nikita <nkorobkov@astralinux.ru>
2024-06-02 13:44:11 +0300

    fix flake8

--- alice/utils/base_functions.py
+++ alice/utils/base_functions.py
@@ -1,8 +1,20 @@
 import aiohttp
 
 
-async def get_response(session: aiohttp.ClientSession, url: str, params: dict = None, method: str = 'GET', cookies: dict = None):
-    async with session.request(method=method.lower(), url=url, params=params, cookies=cookies) as response:
+async def get_response(
+    session: aiohttp.ClientSession,
+    url: str,
+    params: dict = None,
+    method: str = 'GET',
+    cookies: dict = None
+):
+    async with session.request(
+        method=method.lower(),
+        url=url,
+        params=params,
+        cookies=cookies
+    ) as response:
+
         body = await response.json()
         status = response.status
         return body, status
