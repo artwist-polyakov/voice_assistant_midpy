83bd1e88c54319589dd81dc3d217ca4099d96c66
Aleksandr Poliakov <master@artwist.ru>
2024-06-02 21:33:30 +0300

    удаляет излишние воркеры

--- worker-genres/models/search_request.py
+++
@@ -1,8 +0,0 @@
-from models.base_request import BaseRequest
-
-
-class SearchRequest(BaseRequest):
-    external_user_id: str
-    external_session_id: str
-    external_message_id: str
-    query: str
